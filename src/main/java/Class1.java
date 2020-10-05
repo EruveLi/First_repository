c6d541bb4237f967a8418dbfab123a2d2d6eb66b
Liza <eruveriss@icloud.com>
2020-10-05 10:26:58 +0300

    hello world

--- src/main/java/Class1.java
+++ src/main/java/Class1.java
@@ -1,5 +1,10 @@
 public class Class1 {
     public static void main(String[] args) {
+System.out.print ("Yoyo, World");
+int a = 2;
+int b = 5;
+String c = "Amigo will be the dev";
+System.out.print (a+b+ "then" +c);
 
     }
 }
